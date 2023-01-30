f1fbf062ad928e166b43e8620e7bd76f57bcab7a
juliandp30 <j.parra@rdc-consultores.com>
2023-01-30 08:28:31 -0500

    debug

---
+++ src/debug/index.js
@@ -0,0 +1,13 @@
+var a = "Hello";
+
+function hello() {
+  let b = "Hello World";
+  const c = "Hello World";
+  debugger;
+}
+
+hello();
+
+
+// En los navegadores se puede ingresar esta url: about:blank
+// para probar codigo
